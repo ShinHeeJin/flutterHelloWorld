408bca92df4cfdcb224a70db440ca6dffabe87f0
ShinHeeJin <sisesu92@gmail.com>
2023-02-12 17:04:58 +0900

    feature : Container & BoxDecoration

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 40),
-          child: Column(children: [
-            SizedBox(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            const SizedBox(
               height: 80,
             ),
             Row(
@@ -22,7 +25,7 @@ class App extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Text(
+                    const Text(
                       "Hey, Selena",
                       style: TextStyle(
                           color: Colors.white60,
@@ -37,10 +40,51 @@ class App extends StatelessWidget {
                   ],
                 )
               ],
+            ),
+            const SizedBox(
+              height: 120,
+            ),
+            Text(
+              'Total Balance',
+              style:
+                  TextStyle(color: Colors.white.withOpacity(0.8), fontSize: 18),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              '\$5 194 382',
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45)),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 50,
+                    ),
+                    child: Text(
+                      'Transfer',
+                      style: TextStyle(fontSize: 22),
+                    ),
+                  ),
+                ),
+              ],
             )
           ]),
         ),
-        backgroundColor: Color(0xFF181818),
+        backgroundColor: const Color(0xFF181818),
       ),
     );
   }
