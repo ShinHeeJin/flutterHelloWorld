f17d390e700359e776ef685b97d6bcc6af8ee4b0
ShinHeeJin <sisesu92@gmail.com>
2023-02-18 23:29:39 +0900

    feature : Image.network

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:first/screens/today_toons_home_screen.dart';
-import 'package:first/services/api_service.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  ApiService().getTodayToons();
   runApp(const MyApp());
 }
 
--- lib/screens/today_toons_home_screen.dart
+++ lib/screens/today_toons_home_screen.dart
@@ -27,14 +27,11 @@ class TodayToonsHomeScreen extends StatelessWidget {
           future: webtoons,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) =>
-                    Text(snapshot.data![index].title),
-                itemCount: snapshot.data!.length,
-                separatorBuilder: (context, index) => const SizedBox(
-                  width: 10,
-                ),
+              return Column(
+                children: [
+                  const SizedBox(height: 50),
+                  Expanded(child: makeWebToonListView(snapshot)),
+                ],
               );
             }
             return const Center(
@@ -43,6 +40,44 @@ class TodayToonsHomeScreen extends StatelessWidget {
           },
         ),
       ),
+    );
+  }
+
+  ListView makeWebToonListView(AsyncSnapshot<List<WebToonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 10,
+      ),
+      itemBuilder: (context, index) {
+        final webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.4),
+                    ),
+                  ]),
+              child: Image.network(webtoon.thumb),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              webtoon.title,
+              style: const TextStyle(fontSize: 22),
+            ),
+          ],
+        );
+      },
+      itemCount: snapshot.data!.length,
+      separatorBuilder: (context, index) => const SizedBox(width: 15),
     );
   }
 }
