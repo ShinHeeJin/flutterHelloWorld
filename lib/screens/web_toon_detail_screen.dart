1612b4092afb0d927f12e2cf7a1063f11af84147
ShinHeeJin <sisesu92@gmail.com>
2023-02-20 01:25:05 +0900

    feature : WebToon DetailView

--- lib/screens/web_toon_detail_screen.dart
+++ lib/screens/web_toon_detail_screen.dart
@@ -1,13 +1,31 @@
 import 'package:first/models/webtoon.dart';
+import 'package:first/models/webtoon_detail.dart';
+import 'package:first/models/webtoon_senario.dart';
+import 'package:first/services/api_service.dart';
 import 'package:flutter/material.dart';
 
-class WebToonDetailScreen extends StatelessWidget {
+class WebToonDetailScreen extends StatefulWidget {
   final WebToonModel webtoon;
 
   const WebToonDetailScreen({
     super.key,
     required this.webtoon,
   });
+
+  @override
+  State<WebToonDetailScreen> createState() => _WebToonDetailScreenState();
+}
+
+class _WebToonDetailScreenState extends State<WebToonDetailScreen> {
+  late Future<WebtoonDetailModel> webtoonDetail;
+  late Future<List<WebtoonSenarioModel>> webtoonSenarios;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoonDetail = ApiService.getWebtoonDetailById(widget.webtoon.id);
+    webtoonSenarios = ApiService.getWebtoonSenariosById(widget.webtoon.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,34 +36,118 @@ class WebToonDetailScreen extends StatelessWidget {
         foregroundColor: Colors.green,
         backgroundColor: Colors.white,
         title: Text(
-          webtoon.title,
+          widget.webtoon.title,
           style: const TextStyle(
             fontSize: 20,
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 50),
-            Hero(
-              tag: webtoon.id,
-              child: Container(
-                width: 250,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        offset: const Offset(10, 10),
-                        color: Colors.black.withOpacity(0.4),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(50),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Hero(
+                      tag: widget.webtoon.id,
+                      child: Container(
+                        width: 250,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 15,
+                                offset: const Offset(10, 10),
+                                color: Colors.black.withOpacity(0.4),
+                              ),
+                            ]),
+                        child: Image.network(widget.webtoon.thumb),
                       ),
-                    ]),
-                child: Image.network(webtoon.thumb),
-              ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                FutureBuilder(
+                  future: webtoonDetail,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            snapshot.data!.about,
+                            style: const TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            "${snapshot.data!.genre} / ${snapshot.data!.age}",
+                            style: const TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      );
+                    }
+                    return const Text("...");
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                FutureBuilder(
+                  future: webtoonSenarios,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        children: [
+                          for (var senario in snapshot.data!)
+                            Container(
+                              margin: const EdgeInsets.only(bottom: 10),
+                              decoration: BoxDecoration(
+                                color: Colors.green.shade400,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 10,
+                                  horizontal: 20,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      senario.title,
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                    ),
+                                    const Icon(
+                                      Icons.chevron_right_rounded,
+                                      color: Colors.white,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                        ],
+                      );
+                    }
+                    return Container();
+                  },
+                )
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
